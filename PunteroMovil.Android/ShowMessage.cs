36106318dcbca18a4358ef27762adeca698f6be7
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2020-03-17 19:00:03 -0400

    Initial commit

---
+++ PunteroMovil.Android/ShowMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using PunteroMovil.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MessageAndroid))]
+namespace PunteroMovil.Droid
+{
+    public interface IMessage
+    {
+        void LongAlert(string message);
+        void ShortAlert(string message);
+    }
+    public class MessageAndroid : IMessage
+    {
+        public void LongAlert(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Long).Show();
+        }
+
+        public void ShortAlert(string message)
+        {
+            Toast.MakeText(Application.Context, message, ToastLength.Short).Show();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PunteroMovil
+ Esta es una aplicación que permite controlar la presentación de Power Point desde tu celular, siempre y cuando están conectados a la misma red wifi, sirve si el teléfono pasa wifi
